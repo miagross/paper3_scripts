06d313d0fdb494e0ee2af5225e9a7e42160b88a0
miagross <mia.gross89@gmail.com>
2018-07-06 16:37:35 +1000

    adding net radiation calculations

--- cdo_shift_time.sh
+++ cdo_shift_time.sh
@@ -9,8 +9,9 @@ declare -a Variable=("rlds" "rlus" "rsus" "rsds")
 #done 
 
 for iDat in "${Model[@]}";do
- for iVar in "${Variable[@]}";do
-  cdo remapbil,grid_2.5x2.5.txt "/srv/ccrc/data01/z5026024/radiation_data/"$iVar"_day_"$iDat"_1950-2099.nc" \
- 		        "/srv/ccrc/data01/z5026024/radiation_data/"$iVar"_day_"$iDat"_1950-2099_2.5x2.5.nc"
- done
+ #for iVar in "${Variable[@]}";do
+  cdo sub "/srv/ccrc/data01/z5026024/radiation_data/rsds_day_"$iDat"_1950-2099_masked_2.5x2.5.nc" \
+ 		        "/srv/ccrc/data01/z5026024/radiation_data/rsus_day_"$iDat"_1950-2099_masked_2.5x2.5.nc" \
+			"/srv/ccrc/data01/z5026024/radiation_data/Knet_day_"$iDat"_1950-2099_masked_2.5x2.5.nc"
+ #done
 done 
